0829363b0e1af432ca117f18a8055ffaee0319b9
Kleida10 <56648839+Kleida10@users.noreply.github.com>
2019-11-02 12:58:06 -0500

    Changed the  tutorial videos

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tutorial.component.scss']
 })
 export class TutorialComponent implements OnInit {
+  showVideo1: any = false;
+  showVideo2: any = false;
+  showVideo3: any = false;
+  showVideo4: any = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.toggleVideo(3);
   }
 
+  toggleVideo(video: number) {
+    this.showVideo1 = false;
+    this.showVideo2 = false;
+    this.showVideo3 = false;
+    this.showVideo4 = false;
+    if (video == 1) {
+      this.showVideo1 = true;
+    } else if (video == 2) {
+      this.showVideo2 = true;
+    } else if (video == 3) {
+      this.showVideo3 = true;
+    } else if (video == 4) {
+      this.showVideo4 = true;
+    }
+  }
 }
