8eff0ff2879db14743bdf4b61a032fc05de74d44
Violeta <vvalle@ameren.com>
2019-11-02 13:15:54 -0500

    Violeta
    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     UserComponent,
-    RegisterComponent
+    RegisterComponent,
+    
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
